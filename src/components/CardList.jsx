13be1a6870f5e4ce81b7867c041bcb710250c9b2
Hader0 <108206953+Hader0@users.noreply.github.com>
2024-10-08 18:07:46 +1000

    Fixed the styling for the favourite icon

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -3,56 +3,72 @@ import starIcon from '../assets/star.png';
 import starIcon2 from '../assets/star2.png';
 
 function CardList({ newCards }) {
-    // State to track which card is being hovered
-    const [hoveredCardIndex, setHoveredCardIndex] = useState(null);
+    const [mouseOver, setMouseOver] = useState(null);  // To track hovered card
+    const [favouriteCards, setFavouriteCards] = useState([]); // State for favourite cards
+
+    // Function to handle adding/removing favourites
+    const handlefavouriteClick = (card) => {
+        setFavouriteCards((prevfavourites) => {
+            // Check if the card is already in the favourites
+            const isfavourite = prevfavourites.some(favCard => favCard.id === card.id);
+
+            // If already favourite, remove it, otherwise, add it
+            if (isfavourite) {
+                console.log(favouriteCards);
+                return prevfavourites.filter(favCard => favCard.id !== card.id);
+            } else {
+                console.log(favouriteCards);
+                return [...prevfavourites, card];
+            }
+        });
+    };
 
     if (newCards.length > 0) {
-        return newCards.map((card, index) => (
-            <div
-                key={index}
-                className="cardSingle"
-                onMouseOver={() => setHoveredCardIndex(index)} // Set hover state to this card's index
-                onMouseOut={() => setHoveredCardIndex(null)}   // Reset hover state when not hovering
-            >
-                {/* Displaying the Card's Pokemon name */}
-                <h3 className="cardTitle">{card.name}</h3>
+        return newCards.map((card, index) => {
+            // Check if the current card is a favourite inside the map
+            const isFavourite = favouriteCards.some(favCard => favCard.id === card.id);
 
-                {/* Displaying the Card's Pokemon rarity */}
-                <h5 className="rarity">{card.rarity}</h5>
+            return (
+                <div key={index} className="cardSingle">
+                    {/* Displaying the Card's Pokemon name */}
+                    <h3 className="cardTitle">{card.name}</h3>
 
-                {/* Displaying the image of the card */}
-                <img src={card.images.small} alt={card.name} />
+                    {/* Displaying the Card's Pokemon rarity */}
+                    <h5 className="rarity">{card.rarity}</h5>
 
-                <div className="priceFavourite">
-                    {/* Displaying the card price, multiplied by 1.61 */}
-                    {card.cardmarket && card.cardmarket.prices ? (
-                        <>
-                            <h4 className="price">
-                                Price: ${(card.cardmarket.prices.averageSellPrice * 1.61).toFixed(2)}
-                            </h4>
-                            {/* Display different star icon depending on hover state */}
-                            <img
-                                src={hoveredCardIndex === index ? starIcon2 : starIcon} // Check if the current card is being hovered
-                                alt="Favourite"
-                                className="favouriteIcon"
-                            />
-                        </>
-                    ) : (
-                        <>
+                    {/* Displaying the image of the card */}
+                    <img src={card.images.small} alt={card.name} />
+
+                    <div className="priceFavourite">
+                        {/* Displaying the card price, multiplied by 1.61 */}
+                        {card.cardmarket && card.cardmarket.prices ? (
+                            <>
+                                <h4 className="price">
+                                    Price: ${(card.cardmarket.prices.averageSellPrice * 1.61).toFixed(2)}
+                                </h4>
+
+                                <button
+                                    className={`favouriteButton ${isFavourite ? 'favouriteActive' : ''}`}
+                                    onMouseOver={() => setMouseOver(index)} 
+                                    onMouseOut={() => setMouseOver(null)}
+                                    onClick={() => handlefavouriteClick(card)} // Click to toggle favourite
+                                >
+                                    <img 
+                                        src={mouseOver === index ? starIcon : (isFavourite ? starIcon2 : starIcon)}
+                                        className="favouriteIcon"
+                                        alt="Favourite Icon" // Added alt for accessibility
+                                    />
+                                </button>
+
+                            </>
+                        ) : (
                             <h4 className="priceNull">Price Not Available</h4>
-                            {/* Display different star icon depending on hover state */}
-                            <img
-                            src={hoveredCardIndex === index ? starIcon2 : starIcon} // Check if the current card is being hovered
-                            alt="Favourite"
-                            className="favouriteIcon"
-                            />
-                        </>
-                    )}
+                        )}
+                    </div>
                 </div>
-            </div>
-        ));
+            );
+        });
     } else {
-        // For when there are no objects in the newCards (state) array
         return (
             <h4 className="invalidEntry">
                 TIP: You have not entered a valid Pokémon, please make sure that only alphanumeric characters are being used...
